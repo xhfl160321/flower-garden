9bf6ad1be30a354efb0aabb59b783531a4b8dd8e
sueng0422 <sueng0422@naver.com>
2023-09-18 23:44:24 +0900

    commit jq

--- script/script.js
+++ script/script.js
@@ -82,4 +82,12 @@
         }
     });
 
+    //클릭 시 부드러운 scroll down
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 })(jQuery);
